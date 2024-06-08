22b20bf1e277b8756311735f5c4f9e6adc142f6e
Anasch113 <anaschaudry2002@gmail.com>
2024-06-08 19:49:28 +0500

    commit may 8 2024

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 
 import React, { useEffect, useState } from 'react'
 
-import SpeechRecognition, { useSpeechRecognition, resetTranscript } from 'react-speech-recognition';
-import { SpeechConfig, AudioConfig, SpeechRecognizer, ResultReason } from 'microsoft-cognitiveservices-speech-sdk';
+
+import { SpeechConfig, AudioConfig, SpeechRecognizer, ResultReason, SpeechSynthesizer, SpeakerAudioDestination } from 'microsoft-cognitiveservices-speech-sdk';
 import "./App.css"
 function App() {
 
-  const synth = window.speechSynthesis;
+
   const [transcript, setTranscript] = useState('');
   const [isListening, setIsListening] = useState(false);
   const [recognizer, setRecognizer] = useState(null);
+  const [synthesizer, setSynthesizer] = useState(null);
+  const [speechQueue, setSpeechQueue] = useState([]);
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const [isConversation, setIsConversation] = useState(false);
+  const [shouldStartListening, setShouldStartListening] = useState(false);
+
+  const azureKey = process.env.REACT_APP_AZURE_KEY;
+  const azureRegion = process.env.REACT_APP_AZURE_REGION;
+  const botId = process.env.REACT_APP_BOT_KEY_ID;
+  const botClientId = process.env.REACT_APP_BOT_CLIENT_ID;
 
 
 
@@ -22,45 +32,139 @@ function App() {
 
     script.onload = () => {
       window.botpressWebChat.init({
-        botId: 'babd50aa-97f1-4941-9a13-6b0e3c12c18a',
+        botId: botId,
         hostUrl: 'https://cdn.botpress.cloud/webchat/v1',
         messagingUrl: 'https://messaging.botpress.cloud',
-        clientId: 'babd50aa-97f1-4941-9a13-6b0e3c12c18a',
+        clientId: botClientId,
+        botName: 'Fairy Tales ',
+        enableConversationDeletion: true,
+
+
       })
 
-      window.botpressWebChat.onEvent(
-        (event) => {
-          if (event.type === 'MESSAGE.RECEIVED') {
-            console.log("event", event)
-            const message = event.value.payload;
-            console.log('A new message was received!')
-            if (message && message.text) {
-              // Call the speak function for TTS
-              speakText(message.text);
-            }
-          }
-        },
-        ['MESSAGE.RECEIVED']
-      )
+      console.log("connected")
+       // Listen for chat interface loaded event
+   
 
+
+
+      
+      // Combined event handler
+    window.botpressWebChat.onEvent(
+      (event) => {
+        if (event.type === 'LIFECYCLE.READY') {
+          console.log("Chat interface ready", event);
+          // Simulate user message to trigger bot response after interface loaded
+          const initialMessage = "Conversation Started";
+          window.botpressWebChat.sendPayload({
+            type: 'text',
+            text: initialMessage,
+          });
+        } else if (event.type === 'MESSAGE.RECEIVED') {
+
+          console.log("A new message was received", event);
+          const message = event.value.payload;
+          setIsConversation(true);
+          if (message && message.text) {
+            // Call the speak function for TTS
+            setSpeechQueue((prevQueue) => [...prevQueue, message.text]);
+          }
+        }
+      },
+      ['LIFECYCLE.READY', 'MESSAGE.RECEIVED']
+    );
+      
     }
 
 
   }, [])
 
-  // TTS Integration
 
-  function speakText(text) {
-    window.responsiveVoice.speak(text)
+
+
+const startConversation = ()=>{
+
+}
+
+  // Initialize Azure Speech Synthesizer
+  useEffect(() => {
+    if (azureKey && azureRegion) {
+      const speechConfig = SpeechConfig.fromSubscription(azureKey, azureRegion);
+      const audioConfig = AudioConfig.fromDefaultSpeakerOutput();
+      const newSynthesizer = new SpeechSynthesizer(speechConfig, audioConfig);
+      setSynthesizer(newSynthesizer);
+
+      return () => {
+        newSynthesizer.close();
+      };
+    }
+  }, [azureKey, azureRegion]);
+  // TTS Integration
+  const speakText = (text) => {
+    if (synthesizer) {
+      setIsSpeaking(true);
+      synthesizer.speakTextAsync(
+        text,
+        (result) => {
+          if (result.reason === ResultReason.SynthesizingAudioCompleted) {
+            console.log('Speech synthesized for text [' + text + ']');
+            // Using a delay to ensure TTS completes speaking before starting STT
+            setTimeout(() => {
+              setIsSpeaking(false);
+              setShouldStartListening(true);
+            }, text.length * 100); // Adjust timing based on average speech rate
+          } else {
+            console.error('Speech synthesis canceled, ' + result.errorDetails);
+            setIsSpeaking(false);
+          }
+        },
+        (err) => {
+          console.trace('Error synthesizing speech:', err);
+          setIsSpeaking(false);
+        }
+      );
+    }
+  };
+
+
+  console.log("isSpeaking", isSpeaking)
+
+  // Effect to process the speech queue
+  useEffect(() => {
+    if (!isSpeaking && speechQueue.length > 0 && isConversation) {
+      stopListening()
+      const nextText = speechQueue[0];
+      console.log("next text", nextText)
+      setSpeechQueue((prevQueue) => prevQueue.slice(1));
+      speakText(nextText);
+
+    }
+
     
-  }
+  }, [isSpeaking, speechQueue, isConversation]);
+
+
+   // Effect to start listening after TTS completes all messages
+   useEffect(() => {
+    if (shouldStartListening && !isSpeaking && speechQueue.length === 0 && !isListening) {
+      startListening();
+      setShouldStartListening(false);
+    }
+  }, [shouldStartListening, isSpeaking, speechQueue, isListening]);
+
+  console.log("isListening", isListening)
+
+
+
 
   // STT Integration
-
   const startListening = () => {
+    if (isSpeaking || isListening) {
+      return;
+    }
+    setIsListening(true);
 
-    setIsListening(true)
-    const speechConfig = SpeechConfig.fromSubscription('381cba4fbffc487e8e9b47de5887d40d', 'eastus');
+    const speechConfig = SpeechConfig.fromSubscription(azureKey, azureRegion);
     const audioConfig = AudioConfig.fromDefaultMicrophoneInput();
     const recognizer = new SpeechRecognizer(speechConfig, audioConfig);
 
@@ -74,6 +178,7 @@ function App() {
           },
           (err) => {
             console.error('Error stopping recognition:', err);
+
           }
         );
       } else {
@@ -86,7 +191,6 @@ function App() {
           }
         );
         console.log('No speech could be recognized.');
-
       }
     };
 
@@ -113,12 +217,12 @@ function App() {
       setIsListening(false);
     };
 
-    setRecognizer(recognizer)
+    setRecognizer(recognizer);
   };
 
   const stopListening = () => {
     setIsListening(false);
-  
+
     if (recognizer) {
       recognizer.stopContinuousRecognitionAsync(
         () => {
@@ -137,6 +241,7 @@ function App() {
           !isListening ? <button className="btn" onClick={startListening} ><box-icon name='microphone' color='#ffffff'></box-icon> </button> :
             <button className="btn-stop" onClick={stopListening} ><box-icon name='microphone-off' color='#ffffff'></box-icon> </button>
         }
+
 
 
       </div>
